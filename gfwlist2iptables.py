d452fa6dc8e6776e66207b2ce90a530546b0964f
Ye Wang <wangyeye.2008@gmail.com>
2016-12-19 09:47:44 +0800

    Update gfwlist2iptables.py

--- gfwlist2iptables.py
+++ gfwlist2iptables.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 #coding=utf-8
 #
-# Generate a list of dnsmasq rules with ipset for gfwlist
+# Generate a list of iptables rules for gfwlist
 #
-# Copyright (C) 2014 http://www.shuyz.com
-# Ref https://code.google.com/p/autoproxy-gfwlist/wiki/Rules
+
 
 import urllib2
 import re
@@ -17,26 +16,28 @@ import subprocess
 import json
 from IPy import IPSet, IP
 
-mydnsip = '127.0.0.1'
-mydnsport = '5353'
+mydnsip = '192.168.1.1'
+mydnsport = '53'
+myssport = '1090'
+
 # Extra Domain;
 EX_DOMAIN=[ \
-'.google.com', \
-'.google.com.hk', \
-'.google.com.tw', \
-'.google.com.sg', \
-'.google.co.jp', \
-'.google.co.kr', \
-'.blogspot.com', \
-'.blogspot.sg', \
-'.blogspot.hk', \
-'.blogspot.jp', \
-'.blogspot.kr', \
-'.gvt1.com', \
-'.gvt2.com', \
-'.gvt3.com', \
-'.1e100.net', \
-'.blogspot.tw' \
+'google.com', \
+'google.com.hk', \
+'google.com.tw', \
+'google.com.sg', \
+'google.co.jp', \
+'google.co.kr', \
+'blogspot.com', \
+'blogspot.sg', \
+'blogspot.hk', \
+'blogspot.jp', \
+'blogspot.kr', \
+'gvt1.com', \
+'gvt2.com', \
+'gvt3.com', \
+'1e100.net', \
+'blogspot.tw' \
 ]
 
 # the url of gfwlist
@@ -47,18 +48,18 @@ domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
 ip_pattern = re.compile(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b')
 tmpfile = '/tmp/gfwlisttmp'
 # do not write to router internal flash directly
-outfile = '/tmp/dnsmasq_list.conf'
-rulesfile = './dnsmasq_list.conf'
+outfile = '/tmp/iptables_list.conf'
+rulesfile = './iptables_list.conf'
 
 fs =  file(outfile, 'w')
-fs.write('# gfw list ipset rules for dnsmasq\n')
+fs.write('# gfw ip list rules for iptables\n')
 fs.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
 fs.write('#\n')
 
 print 'fetching list...'
 if hasattr(ssl, '_create_unverified_context'):
 	ssl._create_default_https_context = ssl._create_unverified_context
-content = urllib2.urlopen(baseurl, timeout=15).read().decode('base64')
+content = urllib2.urlopen(baseurl, timeout=1500).read().decode('base64')
 
 # write the decoded content to file then read line by line
 tfs = open(tmpfile, 'w')
@@ -70,7 +71,6 @@ print 'page content fetched, analysis...'
 
 # remember all blocked domains, in case of duplicate records
 domainlist = []
-iptables = []
 ret = IPSet()
 
 
@@ -88,43 +88,44 @@ for line in tfs.readlines():
 				if ip_pattern.match(domain[0]):
 					print 'skipping ip: ' + domain[0]
 					continue
-				print 'saving ' + domain[0]
 				domainlist.append(domain[0])
-				bashCommand = 'dig %s'%(domain[0])
-				process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-				output, error = process.communicate()
-				result = output.split('\n')
-				for line in result:
-					if line.startswith(domain[0]):
-						reip = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
-						for ip in reip.findall(line):
-							url = 'https://wq.apnic.net/whois-search/query?searchtext=%s'%(ip)
-							apnic = urllib2.urlopen(url, timeout=15).read()
-							tab = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])/\d+').findall(apnic)
-							iptables.extend(tab)
-							iptables2 = sorted(list(set(iptables)))
-							for ip2 in tab:
-								ret.add(IP(ip2, make_net = True))
-							print iptables2
-				fs.write('server=/.%s/%s#%s\n'%(domain[0],mydnsip,mydnsport))
 		else:
 			print 'no valid domain in this line: ' + line
 
 tfs.close()
+domainlist.extend(EX_DOMAIN)
+domainlist = sorted(list(set(domainlist)))
 
-for each in EX_DOMAIN:
-	fs.write('server=/%s/%s#%s\n'%(each,mydnsip,mydnsport))
+for domain in domainlist:
+	bashCommand = 'dig @%s %s -p%s'%(mydnsip, domain, mydnsport)
+	print bashCommand
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	result = output.split('\n')
+	for line in result:
+		if line.startswith(domain):
+			reip = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
+			for ip in reip.findall(line):
+				print ip
+				try:
+					url = 'https://wq.apnic.net/whois-search/query?searchtext=%s'%(ip)
+					apnic = urllib2.urlopen(url, timeout=1500).read()
+					tab = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])/\d+').findall(apnic)
+					for ip2 in tab:
+						print ip2
+						ret.add(IP(ip2, make_net = True))
+				except:
+					pass
 
-iptables2 = sorted(list(set(iptables)))
-for each in iptables2:
-	fs.write('%s\n'%(each))
+
 for item in ret:
-	fs.write('%s\n'%(item))
+	fs.write('iptables -t nat -A SHADOWSOCKS -p tcp -d %s -j REDIRECT --to-ports %s\n'%(item,myssport))
+
+
 
 print 'write extra domain done'
 
 fs.close();
-print 'moving generated file to dnsmasg directory'
 shutil.move(outfile, rulesfile)
 
 print 'done!'
